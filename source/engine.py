9675e2af417fb79425db3028bc62b4a01cca8373
Jan Juracka <janjuracka861@seznam.cz>
2024-04-17 20:11:45 +0200

    added option to download adaptive files at high res

--- source/engine.py
+++ source/engine.py
@@ -1,14 +1,27 @@
-from pytube import Playlist, YouTube, Channel  # type: ignore
+from pytube import Playlist, YouTube, Channel, Stream, StreamQuery  # type: ignore
 from pathlib import Path
 from dataclasses import dataclass
 from tqdm import tqdm
 from .util import ProgressDownload
+import ffmpeg
+import shutil
 
 
 @dataclass
 class _VideoInfo:
     url: str
     dest_dir: Path
+
+
+SUPPORTED_VIDEO_RESOLUTIONS: list[str] = [
+    '144p', '240p', '360p', '480p', '720p', '1080p', '1440p', '2160p'
+]
+
+
+def _resolution_to_number(res: str) -> int:
+    if res not in SUPPORTED_VIDEO_RESOLUTIONS:
+        return -1
+    return SUPPORTED_VIDEO_RESOLUTIONS.index(res)
 
 
 class Engine:
@@ -30,12 +43,61 @@ class Engine:
         for info in tqdm(self._videos):
             self._init_directory(info.dest_dir)
             yt = YouTube(info.url)
-            stream = yt.streams.get_highest_resolution()
-            ProgressDownload(yt, stream.filesize)
-            stream.download(output_path=info.dest_dir)
+            stream = self._get_highest_resolution(yt.streams)
+            assert stream is not None, "Video stream not found"
+
+            if stream.is_progressive:
+                self._download_stream(yt, stream, info.dest_dir)
+            if stream.is_adaptive:
+                self._download_adaptive(yt, stream, yt.streams, info.dest_dir)
 
     def _init_directory(self, dir: Path) -> None:
         if not dir.exists():
             dir.mkdir(parents=True, exist_ok=True)
         if not dir.is_dir():
             raise RuntimeError('Destination has to be a folder')
+
+    def _download_stream(self, yt: YouTube, stream: Stream, dest_dir: Path) -> Path:
+        ProgressDownload(yt, stream.filesize)
+        return Path(stream.download(output_path=dest_dir))
+
+    def _download_adaptive(self, yt: YouTube, stream: Stream, streams: StreamQuery, dest_dir: Path) -> Path:
+        old_video_path = video_path = self._download_stream(
+            yt, stream, dest_dir)
+        dest_path = video_path.with_suffix('.mkv')
+        video_path = video_path.with_stem('video')
+        shutil.move(old_video_path, video_path)
+
+        audio_stream = self._get_highest_bitrate_audio(streams)
+        assert audio_stream is not None, "Audio stream not found"
+        old_audio_path = audio_path = self._download_stream(
+            yt, audio_stream, dest_dir)
+        audio_path = audio_path.with_stem('audio')
+        shutil.move(old_audio_path, audio_path)
+
+        inp_video = ffmpeg.input(str(video_path))
+        inp_audio = ffmpeg.input(str(audio_path))
+        ffmpeg.concat(inp_video, inp_audio, a=1, v=1) \
+            .output(str(dest_path)).run()
+
+        video_path.unlink()
+        audio_path.unlink()
+        shutil.move(dest_path, video_path)
+        return video_path
+
+    def _get_highest_bitrate_audio(self, query: StreamQuery) -> Stream | None:
+        streams = sorted(
+            query.filter(only_audio=True),
+            key=lambda stream: stream.bitrate, reverse=True)
+
+        if len(streams) == 0:
+            return None
+        return streams[0]
+
+    def _get_highest_resolution(self, query: StreamQuery) -> Stream | None:
+        streams = sorted(
+            query, key=lambda stream: _resolution_to_number(stream.resolution), reverse=True)
+
+        if len(streams) == 0:
+            return None
+        return streams[0]
--- source/util.py
+++ source/util.py
@@ -1,43 +1,75 @@
 from pytube import YouTube, Stream  # type: ignore
 from pathlib import Path
 from datetime import datetime
+from typing import Callable
 
 
-class ProgressDownload:
+class DownloadCallbackWrapper:
     def __init__(self, yt: YouTube, total_size_b: int) -> None:
-        self._yt = yt.register_on_progress_callback(self._on_progress_update)
-        self._yt = yt.register_on_complete_callback(self._on_complete_update)
-        self._total_size_b = total_size_b
-        self._print_progress(0.0, 0.0)
+        self._yt = yt.register_on_progress_callback(self._on_progress_cb)
+        self._yt = yt.register_on_complete_callback(self._on_complete_cb)
+        self._on_progress_cbs: list[Callable[[float, float], None]] = []
+        self._on_complete_cbs: list[Callable[[], None]] = []
+        self._total_size_b = self._delta_remaining_bytes = total_size_b
         self._delta_start = datetime.now()
-        self._delta_remaining_bytes = 0.0
 
-    def _on_progress_update(self, stream: Stream, chunk: bytes, remaning_bytes: int) -> None:
-        self._clear_row()
+    def register_on_progress_callback(self, cb: Callable[[float, float], None]) -> None:
+        """cb ([0...1, MB/s])"""
+        self._on_progress_cbs.append(cb)
+
+    def register_on_complete_callback(self, cb: Callable[[], None]) -> None:
+        self._on_complete_cbs.append(cb)
+
+    def _on_progress_cb(self, stream: Stream, chunk: bytes, remaning_bytes: int) -> None:
         delta_t = datetime.now() - self._delta_start
         delta_mb = (self._delta_remaining_bytes - remaning_bytes) / 10**6
 
-        self._print_progress((1.0 - (remaning_bytes/self._total_size_b)) * 100.0, max(delta_mb / delta_t.total_seconds(), 0.0))
+        for cb in self._on_progress_cbs:
+            cb(1.0 - (remaning_bytes/self._total_size_b),
+               delta_mb / delta_t.total_seconds())
 
         self._delta_start = datetime.now()
         self._delta_remaining_bytes = remaning_bytes
 
-    def _on_complete_update(self, stream: Stream, path: Path) -> None:
+    def _on_complete_cb(self, stream: Stream, path: Path) -> None:
+        for cb in self._on_complete_cbs:
+            cb()
+
+
+class ProgressDownload:
+    def __init__(self, yt: YouTube, total_size_b: int) -> None:
+        self._wrapp = DownloadCallbackWrapper(yt, total_size_b)
+        self._last_print_size = 0
+        self._total_size_mb = total_size_b / 10**6
+        self._print_progress(0.0, 0.0)
+
+        self._wrapp.register_on_progress_callback(self._on_progress_cb)
+        self._wrapp.register_on_complete_callback(self._on_complete_cb)
+
+    def _on_progress_cb(self, pct: float, speed: float) -> None:
+        self._clear_row()
+        self._print_progress(pct * 100.0, speed)
+
+    def _on_complete_cb(self) -> None:
         self._clear_row()
 
     def _clear_row(self) -> None:
-        print('\r', end='')
+        print('\r', end='', flush=True)
+        print(' ' * (self._last_print_size + 1), end='')
+        print('\r', end='', flush=True)
 
     def _print_progress(self, perc: float, speed: float) -> None:
         """
         perc: [0.0 ... 100.0]
         speed: MB/s
         """
-        print(self._progress_to_string(perc, speed), end='', flush=True)
+        string = self._progress_to_string(perc, speed)
+        self._last_print_size = len(string)
+        print(string, end='', flush=True)
 
     def _progress_to_string(self, perc: float, speed: float) -> str:
         """
         perc: [0.0 ... 100.0]
         speed: MB/s
         """
-        return f'{perc:.2f} ({speed} MB/s)'
+        return f'{perc:.2f} % [{self._total_size_mb * perc / 100.0:.2f} of {self._total_size_mb:.2f} MB] ({speed:.3f} MB/s)'
